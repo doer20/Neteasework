6eb9ee57f4796c5b7139e6b2da5781c7c9d79690
doer <375916372@qq.com>
2019-03-23 17:32:30 +0800

    ver 2.0...

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,3 +1,5 @@
+mvn package
+
 cp ./service-registry/target/service-registry-0.0.1-SNAPSHOT.jar ./JARs
 cp ./account-service/target/account-service-0.0.1-SNAPSHOT.jar ./JARs
 cp ./product-service/target/product-service-0.0.1-SNAPSHOT.jar ./JARs
@@ -8,13 +10,4 @@ cp ./edge-service/target/edge-service-0.0.1-SNAPSHOT.jar ./JARs
 cp ./hystrix-dashboard/target/hystrix-dashboard-0.0.1-SNAPSHOT.jar ./JARs
 cp ./turbine/target/turbine-0.0.1-SNAPSHOT.jar ./JARs
 
-java -jar ./JARs/service-registry-0.0.1-SNAPSHOT.jar -spring.profiles.active=peer1 > service-registry-peer1.log 2>&1 &
-java -jar ./JARs/service-registry-0.0.1-SNAPSHOT.jar -spring.profiles.active=peer2 > service-registry-peer2.log 2>&1 &
-sh ./wait-for-service.sh localhost 8761 ./JARs/account-service-0.0.1-SNAPSHOT.jar account-service.log
-sh ./wait-for-service.sh localhost 8761 ./JARs/product-service-0.0.1-SNAPSHOT.jar product-service.log
-sh ./wait-for-service.sh localhost 8761 ./JARs/cart-service-0.0.1-SNAPSHOT.jar cart-service.log
-sh ./wait-for-service.sh localhost 8761 ./JARs/order-service-0.0.1-SNAPSHOT.jar order-service.log
-sh ./wait-for-service.sh localhost 8761 ./JARs/payment-service-0.0.1-SNAPSHOT.jar payment-service.log
-sh ./wait-for-service.sh localhost 8761 ./JARs/edge-service-0.0.1-SNAPSHOT.jar edge-service.log
-sh ./wait-for-service.sh localhost 8761 ./JARs/hystrix-dashboard-0.0.1-SNAPSHOT.jar hystrix-dashboard.log
-sh ./wait-for-service.sh localhost 8761 ./JARs/turbine-0.0.1-SNAPSHOT.jar turbine.log
+sh ./run.sh
